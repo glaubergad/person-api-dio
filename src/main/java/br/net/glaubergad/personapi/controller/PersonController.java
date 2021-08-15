4464fe7f94966e5a2f5aef90dbd1ea441274423c
Glauber Matteis Gadelha <glaubergad@gmail.com>
2021-08-14 23:22:09 -0300

    criadas entidades de persistencia

--- src/main/java/br/net/glaubergad/personapi/controller/PersonController.java
+++ src/main/java/br/net/glaubergad/personapi/controller/PersonController.java
@@ -1,15 +1,34 @@
 package br.net.glaubergad.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/people")
 public class PersonController {
 
     @GetMapping
-    public String getBook(){
+    public String getPeople(){
         return "API Test";
     }
+
+    @GetMapping("/{id}")
+    public String getPersonById(){
+        return "API Test";
+    }
+
+    @PostMapping
+    public String postPerson(){
+        return "API Test";
+    }
+
+    @PutMapping("/{id}")
+    public String putPerson() {
+        return "API Test";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletePerson() {
+        return "API Test";
+    }
+
 }
