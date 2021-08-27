fca6283e1345a7a2515c6c1a0d55ffe83c06df9f
Glauber Gadelha <glauber.gadelha@cinbesa.com.br>
2021-08-27 12:31:08 -0300

    feat: implementado e testado swagger

--- src/main/java/br/net/glaubergad/personapidio/controller/v1/PersonController.java
+++ src/main/java/br/net/glaubergad/personapidio/controller/v1/PersonController.java
@@ -1,9 +1,11 @@
-package br.net.glaubergad.personapidio.controller;
+package br.net.glaubergad.personapidio.controller.v1;
 
 import br.net.glaubergad.personapidio.dto.MessageResponseDto;
 import br.net.glaubergad.personapidio.dto.PersonDto;
 import br.net.glaubergad.personapidio.exception.PersonNotFoundException;
 import br.net.glaubergad.personapidio.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,29 +15,32 @@ import javax.validation.Valid;
 import java.util.List;
 @CrossOrigin
 @RestController
-@RequestMapping("api/v1/people")
+@Api(value = "Person")
+@RequestMapping("v1/people")
 @AllArgsConstructor(onConstructor = @__(@Autowired) )
 public class PersonController {
 
     private final PersonService personService;
 
-
+    @ApiOperation(value ="List all registered people")
     @GetMapping
     public List<PersonDto> getPersonList() {
         return personService.getPersonList();
     }
 
+    @ApiOperation(value ="Get person by given Id")
     @GetMapping("/{id}")
     public PersonDto getPersonById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.getPersonById(id);
     }
-
+    @ApiOperation(value ="Register new person")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDto postPerson(@RequestBody @Valid PersonDto personDto) {
         return personService.createPerson(personDto);
     }
 
+    @ApiOperation(value ="Alter person data")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public MessageResponseDto putPerson(@PathVariable Long id, @RequestBody @Valid PersonDto personDto) throws PersonNotFoundException{
@@ -43,6 +48,7 @@ public class PersonController {
         return personService.putPerson(id,personDto);
     }
 
+    @ApiOperation(value ="Delete person data")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePerson(@PathVariable Long id) throws PersonNotFoundException {
